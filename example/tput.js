fe2d881a08d774189b52649d733c04841c090c20
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-02-16 17:54:20 -0600

    more work

--- example/tput.js
+++ example/tput.js
@@ -4,3 +4,5 @@ var tput = new Tput(process.argv[2] || 'xterm');
 tput.colors();
 
 console.log(tput.info);
+
+tput._compile('%?%p9%t\u001b(0%e\u001b(B%;\u001b[0%?%p6%t;1%;%?%p2%t;4%;%?%p1%p3%|%t;7%;%?%p4%t;5%;%?%p7%t;8%;m');
