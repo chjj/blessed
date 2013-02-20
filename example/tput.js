c1d9868b4989309bfbb4ccd73bc1e9ce3f2a5d6f
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-02-20 07:47:11 -0600

    fixes

--- example/tput.js
+++ example/tput.js
@@ -11,3 +11,4 @@ var tput = new Tput(process.argv[2] || 'xterm', true);
 //tput._compile('%?%p9%t\u001b(0%e\u001b(B%;\u001b[0%?%p6%t;1%;%?%p2%t;4%;%?%p1%p3%|%t;7%;%?%p4%t;5%;%?%p7%t;8%;m');
 
 //console.log(tput.methods.set_attributes([34]) + 'foo' + tput.methods.set_attributes([0]));
+//console.log(tput.set_attributes(34) + 'foo' + tput.set_attributes(0));
