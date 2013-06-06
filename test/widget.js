ed2b9ae4d0da49dd8b2ee5ff27dde28fe0dc6e92
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-06-06 04:03:25 -0500

    major improvements.

--- test/widget.js
+++ test/widget.js
@@ -2,9 +2,29 @@ var blessed = require('blessed')
   , program = blessed()
   , screen;
 
+program.alternateBuffer();
+program.hideCursor();
+
 screen = new blessed.Screen({
   program: program
 });
+
+screen.append(new blessed.Text({
+  screen: screen,
+  parent: screen,
+  top: 0,
+  left: 2,
+  content: 'Welcome to my program'
+}));
+
+screen.append(new blessed.Line({
+  screen: screen,
+  parent: screen,
+  orientation: 'horizontal',
+  top: 1,
+  left: 0,
+  right: 0
+}));
 
 /*
 screen.append(new blessed.Box({
@@ -43,7 +63,7 @@ screen.children[0].append(new blessed.Box({
 }));
 */
 
-screen.append(new blessed.List({
+var list = new blessed.List({
   screen: screen,
   parent: screen,
   fg: 4,
@@ -70,24 +90,96 @@ screen.append(new blessed.List({
     'nine',
     'ten'
   ]
-}));
+});
 
-screen.children[0].prepend(new blessed.Text({
+screen.append(list);
+
+list.prepend(new blessed.Text({
   screen: screen,
-  parent: screen.children[0],
+  parent: list,
   left: 2,
   content: ' My list '
 }));
 
-program.on('keypress', function(ch, key) {
+list.on('keypress', function(ch, key) {
   if (key.name === 'up') {
-    screen.children[0].up();
+    list.up();
     screen.render();
     return;
   } else if (key.name === 'down') {
-    screen.children[0].down();
+    list.down();
     screen.render();
     return;
+  }
+});
+
+var progress = new blessed.ProgressBar({
+  screen: screen,
+  parent: screen,
+  fg: 4,
+  bg: -1,
+  barBg: -1,
+  barFg: 4,
+  border: {
+    type: 'ascii',
+    fg: -1,
+    bg: -1
+  },
+  ch: ':',
+  width: '50%',
+  height: 3,
+  right: 0,
+  bottom: 0,
+  filled: 50
+});
+
+screen.append(progress);
+
+var lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+var stext = new blessed.ScrollableText({
+  screen: screen,
+  parent: screen,
+  content: lorem,
+  fg: 4,
+  bg: -1,
+  barBg: -1,
+  barFg: 4,
+  border: {
+    type: 'ascii',
+    fg: -1,
+    bg: -1
+  },
+  width: '50%',
+  height: 4,
+  left: 0,
+  bottom: 0
+});
+
+screen.append(stext);
+stext.on('keypress', function(ch, key) {
+  if (key.name === 'up') {
+    stext.scroll(-1);
+    screen.render();
+    return;
+  } else if (key.name === 'down') {
+    stext.scroll(1);
+    screen.render();
+    return;
+  }
+});
+
+screen.on('element focus', function(old, cur) {
+  if (old.border) old.border.fg = 0x1ff;
+  if (cur.border) cur.border.fg = 2;
+  screen.render();
+});
+
+program.on('keypress', function(ch, key) {
+  if (key.name === 'tab') {
+    return key.shift
+      ? screen.focusPrev()
+      : screen.focusNext();
   }
   if (key.name === 'escape' || key.name === 'q') {
     program.disableMouse();
@@ -98,7 +190,13 @@ program.on('keypress', function(ch, key) {
   }
 });
 
-program.alternateBuffer();
-program.hideCursor();
+list.focus();
 
 screen.render();
+
+(function fill() {
+  if (progress.filled === 100) progress.filled = 0;
+  progress.progress(2);
+  screen.render();
+  setTimeout(fill, 300);
+})();
