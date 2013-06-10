c9d09547ac9951a0f37b661e3a8436ce446c555b
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-06-09 20:41:25 -0500

    add test content.

--- test/widget.js
+++ test/widget.js
@@ -125,7 +125,7 @@ screen.append(progress);
 
 var lorem = 'Lorem ipsum \x1b[41mdolor sit amet, \nconsectetur adipisicing elit, \x1b[43msed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-var lorem = require('fs').readFileSync(__dirname + '/../t.log', 'utf8');
+var lorem = require('fs').readFileSync(__dirname + '/git.diff', 'utf8');
 
 //lorem = lorem.replace(/\x1b[^m]*m/g, '');
 
