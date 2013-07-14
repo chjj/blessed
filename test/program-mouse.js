3e6a89099e6f59c3ebfa5f0d2491f53c3d5d5459
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-07-14 08:06:47 -0500

    add mouse test.

---
+++ test/program-mouse.js
@@ -0,0 +1,26 @@
+var blessed = require('../')
+  , program = blessed();
+
+// program.setMouse({
+//   allMotion: true,
+//   //utfMouse: true
+//   urxvtMouse: true
+// }, true);
+
+program.alternateBuffer();
+program.enableMouse();
+program.hideCursor();
+
+program.on('mouse', function(data) {
+  program.cup(data.y, data.x);
+  program.write(' ', 'blue bg');
+  program.cup(0, 0);
+  console.log(data);
+});
+
+program.key(['q', 'escape', 'C-c'], function() {
+  program.showCursor();
+  program.disableMouse();
+  program.normalBuffer();
+  process.exit(0);
+});
