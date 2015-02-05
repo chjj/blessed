d70fa6d4a25baa115f9d66a1bdc5e29adbce3767
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-02-04 20:39:34 -0800

    fix wheel, move, and button decoding for mouse events.

--- test/program-mouse.js
+++ test/program-mouse.js
@@ -39,10 +39,16 @@ program.on('keypress', function(ch, data) {
   program.write(util.inspect(data));
 });
 
-program.on('mouse-debug', function(data) {
-  program.cup(20, 0);
-  data = Array.prototype.slice.call(data);
-  program.write(util.inspect(data));
+program.on('mouse-debug', function(s, buf) {
+  if (s) {
+    program.cup(10, 0);
+    program.write(util.inspect(s));
+  }
+  if (buf) {
+    program.cup(11, 0);
+    buf = Array.prototype.slice.call(buf);
+    program.write(util.inspect(buf));
+  }
 });
 
 // program.getCursor(function(err, data) {
