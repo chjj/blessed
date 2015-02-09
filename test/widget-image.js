9e234c39014dbfd689d1695f48ab42699ba85565
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-02-09 00:19:35 -0800

    fix image rendering.

--- test/widget-image.js
+++ test/widget-image.js
@@ -6,15 +6,23 @@ screen = blessed.screen({
   smartCSR: true
 });
 
+blessed.image.w3mdisplay = '/does/not/exist';
+
+var file = process.argv[2] || __dirname + '/test-image.png';
+
 var img = blessed.image({
   parent: screen,
   left: 'center',
   top: 'center',
   width: 'shrink',
   height: 'shrink',
-  bg: 'green',
-  file: process.argv[2] || __dirname + '/test-image.png'
+  bg: 'green'
 });
+
+setTimeout(function() {
+  img.setImage(file);
+  screen.render();
+}, 1000);
 
 img.focus();
 
