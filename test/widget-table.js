91bc189ba3fd6ef4fc78b2f832fe2b8b494a876d
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-03-30 04:58:15 -0700

    add Log and Table elements. update README.

---
+++ test/widget-table.js
@@ -0,0 +1,39 @@
+var blessed = require('../')
+  , screen;
+
+screen = blessed.screen({
+  dump: __dirname + '/logs/table.log',
+  autoPadding: true
+});
+
+var table = blessed.table({
+  parent: screen,
+  top: 'center',
+  left: 'center',
+  data: null,
+  border: 'line',
+  style: {
+    border: {
+      fg: 'red'
+    },
+    header: {
+      fg: 'blue',
+      bold: true
+    },
+    cell: {
+      fg: 'magenta'
+    }
+  }
+});
+
+table.setData([
+  [ 'Animals',  'Foods',  'Times'  ],
+  [ 'Elephant', 'Apple',  '1:00am' ],
+  [ 'Bird',     'Orange', '2:15pm' ]
+]);
+
+screen.key('q', function() {
+  return process.exit(0);
+});
+
+screen.render();
