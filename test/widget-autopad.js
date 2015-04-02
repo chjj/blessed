d6eb6a673f230cfd4521f6a63723884325c55d5f
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-04-02 07:24:25 -0700

    helper.

---
+++ test/widget-autopad.js
@@ -0,0 +1,32 @@
+var blessed = require('../')
+  , screen;
+
+screen = blessed.screen({
+  dump: __dirname + '/logs/autopad.log',
+  smartCSR: true,
+  autoPadding: true
+});
+
+var box1 = blessed.box({
+  parent: screen,
+  top: 'center',
+  left: 'center',
+  width: 20,
+  height: 10,
+  border: 'line'
+});
+
+var box2 = blessed.box({
+  parent: box1,
+  top: 0,
+  left: 0,
+  width: 10,
+  height: 5,
+  border: 'line'
+});
+
+screen.key('q', function() {
+  return process.exit(0);
+});
+
+screen.render();
