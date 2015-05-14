3690aae6959d14c17cfdade367ad57fedca6b3ee
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-05-13 21:00:55 -0700

    test playing and recording.

---
+++ test/widget-play.js
@@ -0,0 +1,13 @@
+var blessed = require('../');
+
+var screen = blessed.screen({
+  dump: __dirname + '/logs/play.log',
+  smartCSR: true
+});
+
+var frames = require(__dirname + '/frames.json');
+
+setInterval(function() {
+  if (!frames.length) return process.exit(0);
+  process.stdout.write(frames.shift());
+}, 100);
---
+++ test/widget-record.js
@@ -0,0 +1,52 @@
+var blessed = require('../')
+  , fs = require('fs');
+
+var screen = blessed.screen({
+  dump: __dirname + '/logs/record.log',
+  smartCSR: true
+});
+
+var btext = blessed.box({
+  parent: screen,
+  left: 'center',
+  top: 'center',
+  width: '80%',
+  height: '80%',
+  style: {
+    bg: 'green'
+  },
+  border: 'line',
+  content: 'CSR should still work.'
+});
+
+var text = blessed.scrollabletext({
+  parent: screen,
+  content: fs.readFileSync(__dirname + '/git.diff', 'utf8'),
+  border: 'line',
+  left: 'center',
+  top: 'center',
+  draggable: true,
+  width: '50%',
+  height: '50%',
+  mouse: true,
+  keys: true,
+  vi: true
+});
+
+text.focus();
+
+screen.key('q', function() {
+  return process.exit(0);
+});
+
+screen.render();
+
+var frames = [];
+
+setInterval(function() {
+  frames.push(screen.screenshot());
+}, 100);
+
+process.on('exit', function() {
+  fs.writeFileSync(__dirname + '/frames.json', JSON.stringify(frames));
+});
