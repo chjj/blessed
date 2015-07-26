901595077fb277d896ad99064fdd5399a54fb36f
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-07-26 13:52:40 -0700

    cleanup browserify transform. fixes #158.

--- browser/transform.js
+++ browser/transform.js
@@ -18,34 +18,17 @@ var requireList = (function() {
 })();
 
 function transform(target) {
-  var data = '';
   var tr = new Transform;
   tr._transform = function(chunk, encoding, callback) {
-    if (!target) {
-      return callback(null, chunk);
-    }
-    data += chunk;
     return callback(null, chunk);
   };
   tr._flush = function(callback) {
-    if (!target) {
-      return callback();
+    if (target) {
+      tr.push(requireList);
     }
-    // tr.push(compile(data));
-    tr.push(requireList);
     return callback();
   };
   return tr;
-}
-
-function compile(data) {
-  var out = '';
-  var names = /widget\.classes = (\[[^\]]+\]);/.exec(data)[1];
-  names = JSON.parse(names.replace(/'/g, '"')).forEach(function(name) {
-    name = name.toLowerCase();
-    out += '\nrequire(\'./widgets/' + name + '\');';
-  });
-  return out;
 }
 
 module.exports = function(file) {
