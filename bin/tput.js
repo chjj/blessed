141c5a6eab36dcb4f267ec3e1e815fa39eee8901
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-07-27 21:46:54 -0700

    minor fix for bin/tput.js.

--- bin/tput.js
+++ bin/tput.js
@@ -7,9 +7,10 @@ var blessed = require('../')
 
 tput = blessed.tput({
   terminal: process.env.TERM,
+  termcap: !!process.env.USE_TERMCAP,
   extended: true
 });
 
 if (tput[cmd]) {
-  process.stdout.write(tput[cmd].apply(tput, argv) + '\n');
+  process.stdout.write(tput[cmd].apply(tput, argv));
 }
