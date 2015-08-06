64072411bbf5fc04bd9a09b6e713cc6072d67ed7
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-08-05 23:30:37 -0700

    fix setTerminal. add termswitch test.

---
+++ test/widget-termswitch.js
@@ -0,0 +1,53 @@
+var blessed = require('../')
+  , screen;
+
+screen = blessed.screen({
+  dump: __dirname + '/logs/termswitch.log',
+  smartCSR: true,
+  warnings: true
+});
+
+var lorem = require('fs').readFileSync(__dirname + '/git.diff', 'utf8');
+
+var btext = blessed.box({
+  parent: screen,
+  left: 'center',
+  top: 'center',
+  width: '80%',
+  height: '80%',
+  style: {
+    bg: 'green'
+  },
+  border: 'line',
+  content: 'CSR should still work.'
+});
+
+var text = blessed.scrollabletext({
+  parent: screen,
+  content: lorem,
+  border: 'line',
+  left: 'center',
+  top: 'center',
+  draggable: true,
+  width: '50%',
+  height: '50%',
+  mouse: true,
+  keys: true,
+  vi: true
+});
+
+text.focus();
+
+screen.key('q', function() {
+  return process.exit(0);
+});
+
+screen.render();
+
+setTimeout(function() {
+  // screen.setTerminal('vt100');
+  screen.terminal = 'vt100';
+  screen.render();
+  text.setContent(screen.program._terminal);
+  screen.render();
+}, 1000);
